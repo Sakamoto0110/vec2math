aacdf7eca950ad772f585effd871975baf2e42dc
Sakamoto0110 <rafael.sakamoto1@hotmail.com>
2024-04-04 05:22:46 -0300

    Added support for concepts
    and moved macro definitions to vec2Math namespace

--- vec2math.h
+++ vec2math.h
@@ -6,6 +6,14 @@
 
 
 
+
+
+
+
+
+
+namespace vec2math {
+
 #ifdef _gcem_HPP
 #define DEFINED
 #define ENABLE_CONSTEXPR
@@ -30,85 +38,110 @@
 
 #ifdef ENABLE_CONSTEXPR
 #define maybe_constexpr constexpr
-static constexpr bool IS_CONSTEXPR_MATH_ENABLED = true;
+    static constexpr bool IS_CONSTEXPR_MATH_ENABLED = true;
 #else
 #define maybe_constexpr
-static constexpr bool IS_CONSTEXPR_MATH_ENABLED = false;
+    static constexpr bool IS_CONSTEXPR_MATH_ENABLED = false;
 #endif
 #undef ENABLE_CONSTEXPR
 #undef DEFINED
+	
+#ifdef __cplusplus 202002L
+#define NO_CONCEPTS
+#endif
+
+	
+#ifdef NO_CONCEPTS
+#define NumericType typename
+#define DecimalType typename
+#else
+
+    template<typename T> concept _NumericType = std::is_arithmetic_v<T>;
+    template<typename T = float> concept _DecimalType = std::is_arithmetic_v<T>;
+	
+#define NumericType _NumericType
+#define DecimalType _DecimalType
+#endif
 
 
-namespace vec2math{
+    
 
 
+    template<DecimalType Decimal_t> constexpr Decimal_t ToDegrees(Decimal_t rad) { return 180 * rad / 3.141592653f; }
+    template<DecimalType Decimal_t> constexpr Decimal_t ToRadians(Decimal_t degrees) { return degrees * 3.141592653f / 180; }
 
-    using decimal_type = float;
-
-
-    constexpr decimal_type ToDegrees(decimal_type rad) { return 180*rad/3.141592653f; }
-    constexpr decimal_type ToRadians(decimal_type degrees){ return degrees*3.141592653f/180; }
-
-
-    template<typename T>
-    class Vec2{
+    
+	
+    template<NumericType T> 
+    class Vec2 {
+        static_assert(std::is_arithmetic_v<T>, "arithmetic type required for Vec2.");
     public:
-
+       
         T x;
         T y;
 
-        constexpr Vec2() noexcept : x(0), y(0)  {}
+        constexpr Vec2() noexcept : x(0), y(0) {}
         constexpr Vec2(T _x, T _y) noexcept : x(_x), y(_y) {}
         constexpr Vec2(const Vec2& vec) noexcept : x(vec.x), y(vec.y) {}
 
-        static constexpr       Vec2 Add(const Vec2& v1, const Vec2& v2)      { return Vec2(v1 + v2); }
-        static constexpr       Vec2 Sub(const Vec2& v1, const Vec2& v2)      { return Vec2(v1 - v2); }
-        static constexpr       Vec2 Mult(const Vec2& v, decimal_type scalar) { return Vec2(v * scalar); }
-        static constexpr       Vec2 Div(const Vec2& v, decimal_type scalar)  { return Vec2(v / scalar); }
-        static maybe_constexpr Vec2 FromAngle(decimal_type theta)            { return Vec2(_cos(theta), _sin(theta)); }
+        static constexpr                                       Vec2 Add(const Vec2& v1, const Vec2& v2) { return Vec2(v1 + v2); }
+        static constexpr                                       Vec2 Sub(const Vec2& v1, const Vec2& v2) { return Vec2(v1 - v2); }
+        template<DecimalType Decimal_t> static constexpr       Vec2 Mult(const Vec2& v, Decimal_t scalar) { return Vec2(v * scalar); }
+        template<DecimalType Decimal_t> static constexpr       Vec2 Div(const Vec2& v, Decimal_t scalar) { return Vec2(v / scalar); }
+        template<DecimalType Decimal_t> static maybe_constexpr Vec2 FromAngle(Decimal_t theta) { return Vec2(_cos(theta), _sin(theta)); }
 
 
-        static constexpr       decimal_type Dot(const Vec2& v1, const Vec2& v2)          { return v1.x*v2.x+v1.y*v2.y; }
-        static constexpr       decimal_type MagSq(const Vec2& v)                         { return v.x*v.x+v.y*v.y; }
-        static maybe_constexpr decimal_type Mag(const Vec2& v)                           { return _sqrt(v.x*v.x+v.y*v.y); }
-        static maybe_constexpr decimal_type Heading(const Vec2& v)                       { return _atan2(v.x,v.y); }
-        static maybe_constexpr decimal_type AngleBetween(const Vec2& v1, const Vec2& v2) { return _acos( v1.dot(v2) / (v1.mag()*v2.mag()) ); }
+        template<DecimalType Decimal_t> static constexpr       Decimal_t Dot(const Vec2& v1, const Vec2& v2) { return v1.x * v2.x + v1.y * v2.y; }
+        template<DecimalType Decimal_t> static constexpr       Decimal_t MagSq(const Vec2& v) { return v.x * v.x + v.y * v.y; }
+        template<DecimalType Decimal_t> static maybe_constexpr Decimal_t Mag(const Vec2& v) { return _sqrt(v.x * v.x + v.y * v.y); }
+        template<DecimalType Decimal_t> static maybe_constexpr Decimal_t Heading(const Vec2& v) { return _atan2(v.x, v.y); }
+        template<DecimalType Decimal_t> static maybe_constexpr Decimal_t AngleBetween(const Vec2& v1, const Vec2& v2) { return _acos(v1.dot(v2) / (v1.mag() * v2.mag())); }
 
 
-        [[maybe_unused]] constexpr Vec2& add(const Vec2& vec)        noexcept { x+=vec.x; y+=vec.y; return *this; }
-        [[maybe_unused]] constexpr Vec2& sub(const Vec2& vec)        noexcept { x-=vec.x; y-=vec.y; return *this; }
-        [[maybe_unused]] constexpr Vec2& mult(decimal_type scalar)   noexcept { x*=scalar; y*=scalar; return *this; }
-        [[maybe_unused]] constexpr Vec2& div(decimal_type scalar)    noexcept { if(scalar != 0) { x /= scalar; y /= scalar; } return *this; }
-        [[maybe_unused]] constexpr Vec2& normalize()                 noexcept { if(decimal_type m = mag(); m != 0) div(m); return *this; }
-        [[maybe_unused]] constexpr Vec2& limit(decimal_type ammount) noexcept { if(decimal_type m = mag(); m != 0) mult((m < ammount ? m : ammount) / m); return *this; }
-        [[maybe_unused]] constexpr Vec2& lerp(const Vec2& vmin,const Vec2& vmax, float t) noexcept{
-            if(t > 0 && t < 1){
-                x = vmin.x+t*(vmax.x-vmin.x);
-                y = vmin.y+t*(vmax.y-vmin.y);
-            }else
-                if(t <= 0) { x = vmin.x; y = vmin.y; } else
-                if(t >= 1) { x = vmax.x; y = vmax.y; }
+        
+        [[maybe_unused]]                                 constexpr Vec2& add(const Vec2& vec)     noexcept { x += vec.x; y += vec.y; return *this; }
+        [[maybe_unused]]                                 constexpr Vec2& sub(const Vec2& vec)     noexcept { x -= vec.x; y -= vec.y; return *this; }
+        template<DecimalType Decimal_t> [[maybe_unused]] constexpr Vec2& normalize()              noexcept { if (Decimal_t m = mag(); m != 0) div(m); return *this; }
+    	template<DecimalType Decimal_t> [[maybe_unused]] constexpr Vec2& mult(Decimal_t scalar)   noexcept { x *= scalar; y *= scalar; return *this; }
+    	template<DecimalType Decimal_t> [[maybe_unused]] constexpr Vec2& div(Decimal_t scalar)    noexcept { if (scalar != 0) { x /= scalar; y /= scalar; } return *this; }
+        template<DecimalType Decimal_t> [[maybe_unused]] constexpr Vec2& limit(Decimal_t ammount) noexcept { if (Decimal_t m = mag(); m != 0) mult((m < ammount ? m : ammount) / m); return *this; }
+        
+        [[maybe_unused]] constexpr Vec2& lerp(const Vec2& vmin, const Vec2& vmax, float t) noexcept {
+            if (t > 0 && t < 1) {
+                x = vmin.x + t * (vmax.x - vmin.x);
+                y = vmin.y + t * (vmax.y - vmin.y);
+            }
+            else
+                if (t <= 0) { x = vmin.x; y = vmin.y; }
+                else
+                    if (t >= 1) { x = vmax.x; y = vmax.y; }
             return *this;
         }
 
-        constexpr decimal_type dot(Vec2 v)                       const noexcept { return x*v.x+y*v.y; }
-        constexpr decimal_type magSq()                           const noexcept { return x*x+y*y; }
-        maybe_constexpr decimal_type mag()                       const noexcept { return _sqrt(x*x+y*y); }
-        maybe_constexpr decimal_type heading()                   const noexcept { return _atan2(x,y); }
-        maybe_constexpr decimal_type angleBetween(const Vec2& v) const noexcept { return _acos( dot(v) / (mag()*v.mag()) ); }
+        template<DecimalType Decimal_t> constexpr Decimal_t dot(Vec2 v)                       const noexcept { return x * v.x + y * v.y; }
+        template<DecimalType Decimal_t> constexpr Decimal_t magSq()                           const noexcept { return x * x + y * y; }
+        template<DecimalType Decimal_t> maybe_constexpr Decimal_t mag()                       const noexcept { return _sqrt(x * x + y * y); }
+        template<DecimalType Decimal_t> maybe_constexpr Decimal_t heading()                   const noexcept { return _atan2(x, y); }
+        template<DecimalType Decimal_t> maybe_constexpr Decimal_t angleBetween(const Vec2& v) const noexcept { return _acos(dot(v) / (mag() * v.mag())); }
 
-
+ 
+        template<typename K> explicit operator K() const { return K{ x,y }; }
 
     };
 
 
 
 
-    template<typename T> static constexpr Vec2<T> operator+(Vec2<T> v1, Vec2<T> v2){ return Vec2(v1.x + v2.x, v1.y + v2.y); }
-    template<typename T> static constexpr Vec2<T> operator-(Vec2<T> v1, Vec2<T> v2){ return Vec2(v1.x - v2.x, v1.y - v2.y); }
-    template<typename T> static constexpr Vec2<T> operator*(Vec2<T> v, decimal_type scalar){ return Vec2(v.x * scalar, v.y * scalar); }
-    template<typename T> static constexpr Vec2<T> operator/(Vec2<T> v, decimal_type scalar){ return Vec2(v.x / scalar, v.y / scalar); }
+    template<NumericType T> static constexpr Vec2<T> operator+(Vec2<T> v1, Vec2<T> v2) { return Vec2(v1.x + v2.x, v1.y + v2.y); }
+    template<NumericType T> static constexpr Vec2<T> operator-(Vec2<T> v1, Vec2<T> v2) { return Vec2(v1.x - v2.x, v1.y - v2.y); }
+    template<NumericType T, DecimalType Decimal_t> static constexpr Vec2<T> operator*(Vec2<T> v, Decimal_t scalar) { return Vec2(v.x * scalar, v.y * scalar); }
+    template<NumericType T, DecimalType Decimal_t> static constexpr Vec2<T> operator/(Vec2<T> v, Decimal_t scalar) { return Vec2(v.x / scalar, v.y / scalar); }
 
+#undef NumericType
+#undef DecimalType
+#ifdef NO_CONCEPTS
+#undef NO_CONCEPTS
+#endif
 
 }
 
